09c542cfe4280458ad514bfb479abae263b706fb
mollnick <maik.gollnick@gmx.de>
2010-06-17 19:06:52 +0200

    - cleanup and added new gesture function calls

--- mindmap2d/extension/firemind@gmx.de/chrome/content/firemindEventDispatcher.js
+++ mindmap2d/extension/firemind@gmx.de/chrome/content/firemindEventDispatcher.js
@@ -2,30 +2,50 @@ Firemind.namespace("touchAPI");
 
 Firemind.touchAPI.EventDispatcher = {
 	
-	_createTouchEvent : function(eventName, touch){
+	_createEvent : function(eventName, touch, args){
 		var touchevt = Firemind.content.document.createEvent("UIEvents").wrappedJSObject;
 	
-		touchevt.initTouchEvent = function(type, canBubble, cancelable, view, detail, x, y, time, id){
+		touchevt.initTouchEvent = function(type, canBubble, cancelable, view, detail, x, y, time, id, args){
 			this.wrappedJSObject.initUIEvent(type, canBubble, cancelable, view, detail);
 			this.wrappedJSObject.x = x;
 			this.wrappedJSObject.y = y;
 			this.wrappedJSObject.time = time;
 			this.wrappedJSObject.id = id;
 			this.wrappedJSObject.targetEl = Firemind.content.document.elementFromPoint(x, y);
+			
+			if(args){
+				for(var key in args){
+					this.wrappedJSObject[key] = args[key];
+				}
+			}
 		};
 
 		touchevt.initTouchEvent(
-			eventName, true, false, Firemind.content, 1, touch.x, touch.y, touch.time, touch.id	
+			eventName, true, false, Firemind.content, 1, touch.x, touch.y, touch.time, touch.id, args
 		);
 		
 		return touchevt;
 	},
 	
 	onTouchEvent : function(eventName, touch){
-		var touchevt = this._createTouchEvent(eventName, touch);
+		var touchevt = this._createEvent(eventName, touch);
 
 		try {  
-			var target = touchevt.targetEl;
+			var target = touchevt.targetEl; 
+			
+			if(target)
+				target.dispatchEvent(touchevt);
+		} catch (e) {
+			Firemind.reportError(e);
+		}
+	},
+	
+	onGestureEvent : function(eventName, touch, args){
+		var touchevt = this._createEvent(eventName, touch, args);
+
+		try {  
+			var target = touchevt.targetEl; 
+			
 			if(target)
 				target.dispatchEvent(touchevt);
 		} catch (e) {
--- mindmap2d/extension/firemind@gmx.de/chrome/content/gesture/firemindFlickGesture.js
+++ mindmap2d/extension/firemind@gmx.de/chrome/content/gesture/firemindFlickGesture.js
@@ -7,65 +7,62 @@ Firemind.touchAPI.FlickGesture = {
 	touchOne : null,
 	touchTwo : null,
 	
-	onFlickIn : null,
-	onFlickOut : null,
-	startDistance : null,
+	distance : null,
 	
-	onBegin : function(touchOne, onFlickIn, onFlickOut){
+	onBegin : function(touchOne){
 		this.isRunning = true;
 		
 		this.touchOne = touchOne;
-	
-		this.onFlickIn = onFlickIn;
-		this.onFlickOut = onFlickOut;
 	},
 	
 	onEnd : function(){
 		this.isRunning = false;
 		this.touchOne = null;
 		this.touchTwo = null;
-		this.startDistance = null;
-		this.onFlickIn = null;
-		this.onFlickOut = null;
+		this.distance = null;
 	},
 	
 	execute : function(currentTouch, touchList){
 	
-		if(this.touchOne == null) return;
+		if(this.touchOne == null || Firemind.touchAPI.TapGesture.isRunning) return;
 	
-		if(this.touchTwo == null){
+		if(this.touchTwo == null && this.touchOne.id != currentTouch.id){
 			
 			this.touchTwo = currentTouch;
 			
 			// calculate start distance
 			var diffX = (this.touchOne.x - this.touchTwo.x);
 			var diffY = (this.touchOne.y - this.touchTwo.y);
-			this.startDistance = Math.sqrt(diffX * diffX + diffY * diffY);
-			
+			this.distance = Math.sqrt(diffX * diffX + diffY * diffY);
+	
 		}else {
 
 			if(this.touchTwo.id == currentTouch.id && touchList[this.touchOne.id] != undefined){
+					
+					var thr = 15;
+					var varX = Math.abs(this.touchTwo.x - currentTouch.x);
+					var varY = Math.abs(this.touchTwo.y - currentTouch.y);
 			
-				this.touchTwo = currentTouch;
-		
-				// calculate distance
-				var diffX = (this.touchOne.x - this.touchTwo.x);
-				var diffY = (this.touchOne.y - this.touchTwo.y);
-				var r = Math.sqrt(diffX * diffX + diffY * diffY);
+					if(varX < thr && varY < thr) return;
+			
+					this.touchTwo = currentTouch;
+			
+					// calculate distance
+					var diffX = (this.touchOne.x - this.touchTwo.x);
+					var diffY = (this.touchOne.y - this.touchTwo.y);
+					var r = Math.sqrt(diffX * diffX + diffY * diffY);
+					
+					// calculate angle
+					var b = Math.abs(diffX);
+					var cos = Math.acos(b / r);
+					
+					if(r < this.distance){
+						Firemind.touchAPI.EventDispatcher.onGestureEvent("onFlickIn", this.touchOne, {radius: r, angle: cos, t2: this.touchTwo.id, t1: this.touchOne.id});
+					}else if(r > this.distance){
+						Firemind.touchAPI.EventDispatcher.onGestureEvent("onFlickOut", this.touchOne, {radius: r, angle: cos, t2: this.touchTwo.id, t1: this.touchOne.id});
+					}
 				
-				// calculate angle
-				var b = Math.abs(diffX);
-				var cos = Math.acos(b / r);
-				
-				if(r < this.startDistance){
-					this.onFlickIn(this.touchOne, r, cos);
-				}else {
-					this.onFlickOut(this.touchOne, r, cos);
-				}
-			
-			}else {
-			
-				this.onEnd();
+					this.distance = r;
 				
 			}
 	
